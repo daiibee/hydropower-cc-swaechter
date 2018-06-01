4d4fd3120e62565f84ec59548058c573b1e5a916
Simon Wächter <waechter.simon@gmail.com>
2018-06-01 14:27:47 +0200

    Add the icons

--- src/main/java/ch/fhnw/cuie/project/toolbar/HydroToolBar.java
+++ src/main/java/ch/fhnw/cuie/project/toolbar/HydroToolBar.java
@@ -1,11 +1,8 @@
 package ch.fhnw.cuie.project.toolbar;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
@@ -15,13 +12,23 @@ import java.io.InputStream;
 
 public class HydroToolBar extends ToolBar {
 
+    private static final String FONT_PATH = "/fonts/fontawesome-webfont.ttf";
+
+    private static final String STYLE_PATH = "toolbar-control.css";
+
+    private int fontSize;
+
     private Font customFont;
+
+    private Button saveButton;
 
     private Button createButton;
 
-    private Button editButton;
-
     private Button deleteButton;
+
+    private Button undoButton;
+
+    private Button redoButton;
 
     private Region horizontalRegionSpacer;
 
@@ -31,111 +38,105 @@ public class HydroToolBar extends ToolBar {
 
     private TextField searchTextField;
 
-    public HydroToolBar() {
-        initializeSelf();
+    public HydroToolBar(int fontSize) {
+        initializeFont(fontSize);
+        initializeStyle();
         initializeParts();
         layoutParts();
     }
 
-    private void initializeSelf() {
-        //String fonts = getClass().getResource("/fonts/fonts.css").toExternalForm();
-        //getStylesheets().add(fonts);
+    private void initializeFont(int fontSize) {
+        try {
+            this.fontSize = fontSize;
+            InputStream fontInputStream = HydroToolBar.class.getResourceAsStream(FONT_PATH);
+            customFont = Font.loadFont(fontInputStream, fontSize);
+        } catch (Exception exception) {
+            throw new RuntimeException("Achtung: Die Datei " + FONT_PATH + " konnte nicht geladen/verarbeitet werden. Bitte füge Sie deinem Projekt hinzu!", exception);
+        }
+    }
 
-        InputStream font = HydroToolBar.class.getResourceAsStream("/fonts//fontawesome-webfont.ttf");
-        customFont = Font.loadFont(font, 14);
-
-        String stylesheet = getClass().getResource("toolbar-control.css").toExternalForm();
-        getStylesheets().add(stylesheet);
-        getStyleClass().add("toolbar-control");
+    private void initializeStyle() {
+        try {
+            String stylesheet = getClass().getResource(STYLE_PATH).toExternalForm();
+            getStylesheets().add(stylesheet);
+            getStyleClass().add("toolbar-control");
+        } catch (Exception exception) {
+            throw new RuntimeException("Achtung: Die Datei " + STYLE_PATH + " konnte nicht geladen/verarbeitet werden. Bitte füge Sie deinem Projekt hinzu!", exception);
+        }
     }
 
     private void initializeParts() {
         horizontalRegionSpacer = new Region();
-        createButton = createButton("create-button", '\uf040');
-        editButton = createButton("edit-button", '\uf040');
-        deleteButton = createButton("delete-button", '\uf040');
-        germanLanguageButton = createButton("german-language-button", '\uf040');
-        englishLanguageButton = createButton("english-language-button", '\uf040');
-        searchTextField = createTextField("search-textfield");
+        saveButton = createButton('\uf0c7');
+        createButton = createButton('\uf067');
+        deleteButton = createButton('\uf00d');
+        undoButton = createButton('\uf0e2');
+        redoButton = createButton('\uf01e');
+        germanLanguageButton = createLanguageButton("DE");
+        englishLanguageButton = createLanguageButton("EN");
+        searchTextField = createTextField();
     }
 
     private void layoutParts() {
-        getItems().clear();
-
-        if (createButton.getOnAction() != null) {
-            getItems().add(createButton);
-        }
-
-        if (editButton.getOnAction() != null) {
-            getItems().add(editButton);
-        }
-
-        if (deleteButton.getOnAction() != null) {
-            getItems().add(deleteButton);
-        }
-
         HBox.setHgrow(horizontalRegionSpacer, Priority.ALWAYS);
-        getItems().add(horizontalRegionSpacer);
-
-        if (germanLanguageButton.getOnAction() != null) {
-            getItems().add(germanLanguageButton);
-        }
-
-        if (englishLanguageButton.getOnAction() != null) {
-            getItems().add(englishLanguageButton);
-        }
-
-        if (searchTextField.getOnAction() != null || searchTextField.getOnKeyTyped() != null) {
-            getItems().add(searchTextField);
-        }
+        getItems().addAll(saveButton, createButton, deleteButton, undoButton, redoButton, horizontalRegionSpacer, germanLanguageButton, englishLanguageButton, searchTextField);
     }
 
-    private Button createButton(String cssName, char iconName) {
+    private Button createButton(char iconName) {
         Button button = new Button();
         button.setText(String.valueOf(iconName));
         button.setFont(customFont);
-        button.getStyleClass().add(cssName);
+        button.getStyleClass().add("button");
         return button;
     }
 
-    private TextField createTextField(String cssName) {
+    private Button createLanguageButton(String languageName) {
+        Button button = new Button();
+        button.setText(languageName);
+        button.setFont(customFont);
+        button.getStyleClass().add("button");
+        return button;
+    }
+
+    private TextField createTextField() {
         TextField textField = new TextField();
-        textField.getStyleClass().add(cssName);
+        textField.getStyleClass().add("textfield");
         return textField;
     }
 
-    public void setCreateButtonOnAction(EventHandler<ActionEvent> event) {
-        createButton.setOnAction(event);
-        layoutParts();
+    public Button getSaveButton() {
+        return saveButton;
     }
 
-    public void setEditButtonOnAction(EventHandler<ActionEvent> event) {
-        editButton.setOnAction(event);
-        layoutParts();
+    public Button getCreateButton() {
+        return createButton;
     }
 
-    public void setDeleteButtonOnAction(EventHandler<ActionEvent> event) {
-        deleteButton.setOnAction(event);
-        layoutParts();
+    public Button getDeleteButton() {
+        return deleteButton;
     }
 
-    public void setGermanLanguageButtonOnAction(EventHandler<ActionEvent> event) {
-        germanLanguageButton.setOnAction(event);
-        layoutParts();
+    public Button getUndoButton() {
+        return undoButton;
     }
 
-    public void setEnglishLanguageButtonOnAction(EventHandler<ActionEvent> event) {
-        englishLanguageButton.setOnAction(event);
-        layoutParts();
+    public Button getRedoButton() {
+        return redoButton;
     }
 
-    public void setSearchTextFieldOnAction(EventHandler<ActionEvent> event) {
-        searchTextField.setOnAction(event);
-        layoutParts();
+    public Region getHorizontalRegionSpacer() {
+        return horizontalRegionSpacer;
     }
 
-    public void setSearchTextFieldOnKeyPressed(EventHandler<KeyEvent> event) {
-        searchTextField.setOnKeyPressed(event);
-        layoutParts();
+    public Button getGermanLanguageButton() {
+        return germanLanguageButton;
+    }
+
+    public Button getEnglishLanguageButton() {
+        return englishLanguageButton;
+    }
+
+    public TextField getSearchTextField() {
+        return searchTextField;
     }
 }
--- src/main/java/ch/fhnw/cuie/project/toolbar/ToolBarApplication.java
+++ src/main/java/ch/fhnw/cuie/project/toolbar/ToolBarApplication.java
@@ -14,14 +14,18 @@ public class ToolBarApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        HydroToolBar toolBar = new HydroToolBar();
-        toolBar.setCreateButtonOnAction(mouseEvent -> System.out.println("Create button clicked!"));
-        toolBar.setEditButtonOnAction(actionEvent -> System.out.println("Edit button clicked!"));
-        toolBar.setDeleteButtonOnAction(actionEvent -> System.out.println("Delete button clicked!"));
-        toolBar.setGermanLanguageButtonOnAction(actionEvent -> System.out.println("German button clicked!"));
-        toolBar.setEnglishLanguageButtonOnAction(actionEvent -> System.out.println("English button clicked!"));
-        toolBar.setSearchTextFieldOnAction(actionEvent -> System.out.println("Search text field submitted!"));
-        toolBar.setSearchTextFieldOnKeyPressed(actionEvent -> System.out.println("Search text field typed!"));
+        HydroToolBar toolBar = new HydroToolBar(18);
+        toolBar.getSaveButton().setOnAction(mouseEvent -> System.out.println("Save button clicked!"));
+        toolBar.getCreateButton().setOnAction(actionEvent -> System.out.println("Create button clicked!"));
+        toolBar.getDeleteButton().setOnAction(actionEvent -> System.out.println("Delete button clicked!"));
+        toolBar.getUndoButton().setOnAction(actionEvent -> System.out.println("Undo button clicked!"));
+        toolBar.getRedoButton().setOnAction(actionEvent -> System.out.println("Redo button clicked!"));
+        toolBar.getGermanLanguageButton().setOnAction(actionEvent -> System.out.println("German button clicked!"));
+        toolBar.getEnglishLanguageButton().setOnAction(actionEvent -> System.out.println("English button clicked!"));
+        toolBar.getSearchTextField().setOnAction(actionEvent -> System.out.println("Search text field submitted!"));
+        toolBar.getSearchTextField().setOnKeyTyped(actionEvent -> System.out.println("Search text field typed!"));
+
+        toolBar.getCreateButton().setDisable(true);
 
         BorderPane borderPane = new BorderPane();
         borderPane.setTop(toolBar);
